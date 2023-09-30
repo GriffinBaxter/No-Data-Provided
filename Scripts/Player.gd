0419c994c7a30c581ff4c57c127213e99145216b
Griffin Baxter <GriffinRBaxter@gmail.com>
2023-09-30 15:37:09 +1300

    Add basic level with first person controller
    Added a basic level with a first person controller. Includes walking, jumping, and camera movement.

---
+++ Scripts/Player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.002
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = lerp(velocity.x, direction.x * SPEED, delta * 12.0)
+			velocity.z = lerp(velocity.z, direction.z * SPEED, delta * 12.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * SPEED, delta * 2.0)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, delta * 2.0)
+
+func _process(delta):
+	move_and_slide()  # Not ideal, should move out of _process eventually
