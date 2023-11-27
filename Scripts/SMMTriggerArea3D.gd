6fc77020c3834168ebe30f0e2f97432e28523a50
Griffin Baxter <GriffinRBaxter@gmail.com>
2023-11-27 20:59:27 +1300

    Add tween camera transition from player to split screen match

--- Scripts/SMMTriggerArea3D.gd
+++ Scripts/SMMTriggerArea3D.gd
@@ -6,6 +6,7 @@ extends Area3D
 
 @onready var player_camera = $"../../Control/AspectRatioContainer/SubViewportContainer/SubViewport/Player/Head/Camera3D"
 @onready var player_head = $"../../Control/AspectRatioContainer/SubViewportContainer/SubViewport/Player/Head"
+@onready var smm_camera = $".."
 
 var player_entered_area = false
 var cutscene_started = false
@@ -13,6 +14,13 @@ var cutscene_started = false
 func _process(_delta):
 	if (!cutscene_started and player_entered_area and is_within_offset_degrees(-25, 10, player_camera.rotation_degrees.x) and is_within_offset_degrees(11.6, 10, player_head.rotation_degrees.y)):
 		cutscene_started = true
+
+		var tween = get_tree().create_tween().set_parallel()
+		tween.tween_property(player_camera, "global_position", smm_camera.position, 0.5)
+		tween.tween_property(player_camera, "global_rotation", smm_camera.rotation, 0.5)
+
+		await get_tree().create_timer(0.5).timeout
+
 		sub_viewport_container.visible = false
 		smm_animation_player.play("cutscene_1")
 
