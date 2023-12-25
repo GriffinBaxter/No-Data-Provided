1e4adad5432b589b96f51d6eb553766a87b8483f
Griffin Baxter <GriffinRBaxter@gmail.com>
2023-12-26 11:25:47 +1300

    GAME-17: Implement return to menu button on pause

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var level = $".."
 @onready var player = $"../Control/AspectRatioContainer/SubViewportContainer/SubViewport/Player"
 
+var main_menu = "res://Scenes/main_menu.tscn"
+
 signal save_player_position
 
 func _on_resume_pressed():
@@ -10,6 +12,9 @@ func _on_resume_pressed():
 
 func _on_save_pressed():
 	emit_signal("save_player_position", [player.position.x, player.position.y, player.position.z])
+
+func _on_return_to_menu_pressed():
+	get_tree().change_scene_to_file(main_menu)
 
 func _on_quit_pressed():
 	get_tree().quit()
