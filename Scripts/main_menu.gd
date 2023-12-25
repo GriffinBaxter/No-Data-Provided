c310fa7b9421d52d3e38b7b034c0b22882e08d58
Griffin Baxter <GriffinRBaxter@gmail.com>
2023-12-26 11:22:30 +1300

    GAME-2: Implement main menu buttons

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@onready var continue_button = $MarginContainer/VBoxContainer/Continue
+
+var level = "res://Scenes/level.tscn"
+
+var save_exists = false
+
+func _ready():
+	update_save_exists()
+
+func update_save_exists():
+	save_exists = FileAccess.file_exists("user://01.save")
+	continue_button.disabled = not save_exists
+
+func _on_continue_pressed():
+	update_save_exists()
+	if save_exists:
+		get_tree().change_scene_to_file(level)
+
+func _on_new_pressed():
+	DirAccess.remove_absolute("user://01.save")
+	get_tree().change_scene_to_file(level)
+
+func _on_quit_pressed():
+	get_tree().quit()
