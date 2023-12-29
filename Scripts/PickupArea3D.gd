daf2cd547e6a7a54a72803185ffb4d5fcf98e7ac
Griffin Baxter <GriffinRBaxter@gmail.com>
2023-12-29 19:03:41 +1300

    GAME-41: Allow table slice to be rotated

--- Scripts/PickupArea3D.gd
+++ Scripts/PickupArea3D.gd
@@ -8,6 +8,7 @@ extends Area3D
 @onready var slicer = $"../Slicer"
 
 var picked_up = false
+var object_rotation = 0
 
 const slicer_offset_position = Vector3(0, 1.5, 0)
 const slicer_offset_rotation_degrees = 20
@@ -18,7 +19,11 @@ func _on_body_entered(body):
 
 func _process(_delta):
 	if picked_up:
+		if Input.is_action_pressed("rotate_left"):
+			object_rotation += 0.02;
+		if Input.is_action_pressed("rotate_right"):
+			object_rotation -= 0.02;
 		table_slice.position = pickup.global_position
 		slicer.position = table_slice.position + slicer_offset_position
-		table_slice.rotation.y = pickup.global_rotation.y
+		table_slice.rotation.y = pickup.global_rotation.y + object_rotation
 		slicer.rotation_degrees.y = table_slice.rotation_degrees.y + slicer_offset_rotation_degrees
