d642b36bd21b8f8e8a538a39ff7604d398d0221c
Griffin Baxter <GriffinRBaxter@gmail.com>
2024-02-25 14:44:24 +1300

    GAME-64: Make identification highlight flash

--- Scripts/viewport.gd
+++ Scripts/viewport.gd
@@ -6,13 +6,36 @@ extends SubViewport
 @onready var player_camera: Camera3D = $"../../../Player/Head/Camera3D"
 @onready var identification: Node3D = $"../../../Identification"
 
+@onready var viewport_identification_mesh: MeshInstance3D = $IdentificationNoShader/Cube
+
+
+func _ready() -> void:
+	const IDENTIFICATION_COLOURS := [Color(1, 1, 1, 0), Color(1, 1, 1, 0.5)]
+	var alternate := false
+	while true:
+		var tween := get_tree().create_tween()
+		tween.tween_method(
+			update_identification_transparency,
+			IDENTIFICATION_COLOURS[1] if alternate else IDENTIFICATION_COLOURS[0],
+			IDENTIFICATION_COLOURS[0] if alternate else IDENTIFICATION_COLOURS[1],
+			1
+		)
+		await get_tree().create_timer(1).timeout
+
+		tween.stop()
+		alternate = !alternate
+
 
 func _process(_delta: float) -> void:
-	match_movement_camera(viewport_camera, player_camera)
+	match_movement_camera()
 	for objects: Array[Variant] in [[viewport_identification, identification]]:
 		var viewport: Variant = objects[0]
 		var to_match: Variant = objects[1]
 		match_movement(viewport, to_match)
+
+
+func update_identification_transparency(value: Color) -> void:
+	viewport_identification_mesh.material_override.albedo_color = value
 
 
 func match_movement(viewport: Variant, to_match: Variant) -> void:
@@ -21,6 +44,6 @@ func match_movement(viewport: Variant, to_match: Variant) -> void:
 	viewport.visible = to_match.visible
 
 
-func match_movement_camera(viewport: Camera3D, to_match: Camera3D) -> void:
-	match_movement(viewport, to_match)
-	viewport.fov = to_match.fov
+func match_movement_camera() -> void:
+	match_movement(viewport_camera, player_camera)
+	viewport_camera.fov = player_camera.fov
